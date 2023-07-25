9c858bf8ac8982a4314ecde2a9d29c55ab2c086b
alitainfotech <ankit@alitainfotech.com>
2023-07-25 13:24:09 +0530

    feat:pagination

--- src/views/Dashboard/Calls.vue
+++ src/views/Dashboard/Calls.vue
@@ -176,8 +176,8 @@
       <nav class="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
         <div class="-mt-px flex w-0 flex-1">
           <a
-            v-if="calls.current_page != 1"
-            @click="callsStore.get_calls(calls.current_page - 1)"
+            v-if="currentPage != 1"
+            @click="handlePageNoClick((currentPage = currentPage - 1))"
             href="#"
             class="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
           >
@@ -186,7 +186,7 @@
           </a>
         </div>
         <div class="hidden md:-mt-px md:flex">
-          <div v-if="calls.data && calls.data.length">
+          <!-- <div v-if="calls.data && calls.data.length">
             <a
               v-for="(page, index) in Math.ceil(calls.total / calls.per_page)"
               :key="index"
@@ -199,8 +199,45 @@
               ]"
               >{{ index + 1 }}</a
             >
+          </div> -->
+          <div v-if="callList && callList.length">
+            <a
+              v-if="currentPage > 3"
+              @click="handlePageNoClick(pages[0])"
+              href="#"
+              :class="[
+                currentPage === pages[0]
+                  ? 'inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600'
+                  : 'inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
+              ]"
+              >{{ pages[0] }}</a
+            >
+            <a v-if="currentPage > 3">....</a>
+            <a
+              v-for="(pageNo, index) in displayedPages"
+              :key="index"
+              @click="handlePageNoClick(pageNo)"
+              href="#"
+              :class="[
+                currentPage === pageNo
+                  ? 'inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600'
+                  : 'inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
+              ]"
+              >{{ pageNo }}</a
+            >
+            <a v-if="currentPage < pages.length - 2">....</a>
+            <a
+              v-if="currentPage < pages.length - 2"
+              @click="handlePageNoClick(pages.length)"
+              href="#"
+              :class="[
+                currentPage === pageNo
+                  ? 'inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600'
+                  : 'inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700'
+              ]"
+              >{{ pages[pages.length - 1] }}</a
+            >
           </div>
-
           <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500
           hover:text-gray-700 hover:border-gray-300" -->
           <!--
@@ -238,8 +275,8 @@
         </div>
         <div class="-mt-px flex w-0 flex-1 justify-end">
           <a
-            v-if="calls.current_page < Math.ceil(calls.total / calls.per_page)"
-            @click="getNextData(calls.current_page + 1)"
+            v-if="currentPage < Math.ceil(total / perPage)"
+            @click="handlePageNoClick((currentPage = currentPage + 1))"
             href="#"
             class="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
           >
@@ -259,9 +296,14 @@ import { useCallsStore } from '../../stores/CallsStore'
 const callsStore = useCallsStore()
 const { loading, calls } = storeToRefs(callsStore)
 import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/vue/20/solid'
+
 const searchQuery = ref('')
 const tableData = ref([])
 const props = defineProps(['searchKey'])
+const pages = ref([])
+const currentPage = ref(1)
+const perPage = ref(20)
+const total = ref(0)
 onMounted(() => {
   getNextData(1)
 
@@ -280,14 +322,54 @@ const callList = computed(() => {
   }
   return tableData
 })
+const displayedPages = computed(() => {
+  // if currentPage is page 1
+  if (currentPage.value === 1) {
+    return pages.value.slice(currentPage.value - 1, currentPage.value + 4)
+  }
+  // if currentPage is the last page
+  else if (currentPage.value === pages.value.length) {
+    return pages.value.slice(currentPage.value - 5, currentPage.value + 1)
+  }
+  // if currentPage is between 4-7
+  else if (currentPage.value >= 4 && currentPage.value <= 7) {
+    return pages.value.slice(currentPage.value - 2, currentPage.value + 1)
+  }
+  // if currentPage more than 7
+  else if (currentPage.value > 7) {
+    return pages.value.slice(currentPage.value - 4, currentPage.value + 1)
+  }
+  // if currentPage less than 4
+  else {
+    return pages.value.slice(currentPage.value - 2, currentPage.value + 3)
+  }
+})
 watch(
   calls,
   (newValue, oldValue) => {
     tableData.value = newValue.data
+    currentPage.value = newValue?.current_page
+    perPage.value = newValue?.per_page
+    total.value = newValue?.total
+    setPages()
   },
   { immediate: true }
 )
+function setPages() {
+  if (total.value) {
+    let numberOfPages = Math.ceil(total.value / perPage.value)
+    //generate 10 pages (100 / 10)
+    pages.value = []
+    for (let i = 1; i <= numberOfPages; i++) {
+      pages.value.push(i)
+    }
+  }
+}
+function handlePageNoClick(pageNo) {
+  currentPage.value = pageNo
+  getNextData(pageNo)
+}
 function getNextData(pageIndex) {
-  callsStore.get_calls(1)
+  callsStore.get_calls(pageIndex)
 }
 </script>
